2deed9c6734d7a53f9080ecd059e8a573ab87691
Andres Vargas <devandresvargas@gmail.com>
2022-09-02 21:18:49 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DataBaseManager',
+    version= '1.0',
+    description='',
+    author='Andres Felipe Vargas',
+    author_email='devandresvargas@gmail.com',
+    url='',
+    packages=['scripts'],
+    scripts=['run.py']
+)
